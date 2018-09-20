240feca02d357b2372e760971f3b8cecdca9490e
Friedrich.Raoul <Friedrich.Raoul@DESKTOP-F5E3DSV.2631.skool.net>
2018-09-20 09:15:11 +0200

    Neue Version Hausaufgabe

--- src/Fizz_Buzz.java
+++ src/Fizz_Buzz.java
@@ -3,7 +3,7 @@ public class Fizz_Buzz {
 
 	public static void main(String[] args) {
 		for(int i = 1; i < 1001; i++ ){
-			if(i % 3 ==0){
+			if(divider3(i)){
 				System.out.println(i+"   Fizz");
 				if(i % 5==0){
 					System.out.println(i + "   Buzz");
@@ -22,5 +22,10 @@ public class Fizz_Buzz {
 		}
 
 	}
+	public static boolean divider3(int i){
+		if (i%3==0){ 
+			return true;
+		}else{return false;}
+		}
 	}
 
