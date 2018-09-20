2d4105b668f11d7ee2c70506c5175ad7dd62ece5
Friedrich.Raoul <Friedrich.Raoul@DESKTOP-F5E3DSV.2631.skool.net>
2018-09-20 08:25:52 +0200

    he

--- src/Problem.java
+++ src/Problem.java
@@ -11,16 +11,16 @@ public class Problem {
     
 	for(int i = 5; i <=100;i++){ //Variable int = 5 wird hinzugefügt und solange hochgezählt bis i 100 erreicht
 			zahl = i;
-		  System.out.print("Seed: "+i+"| ");
+		  System.out.print("Seed: "+i+"| "); //schönere Formatierung der Ausgabe in der Console
 			
-			while(zahl>1){
+			while(zahl>1){ //während die Zahl größer als 1 ist
 		  
-		if(zahl % 2 != 0){
-			zahl = zahl*3+1;
+		if(zahl % 2 != 0){  //wenn man "zahl" durch 2 teilt wird der Rest bestimmt. Wenn der Rest null ist else 
+			zahl = zahl*3+1; //zahl mal 3+1
 			}
-		else{zahl = zahl/2;}
+		else{zahl = zahl/2;} //zahl / 2
 		
-	   System.out.print(zahl+", ");
+	   System.out.print(zahl+", "); 
 	 
 	   
 	   }
