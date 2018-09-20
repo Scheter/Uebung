68ad705282cf263b7c27b5a887510d38656ad26b
Friedrich.Raoul <Friedrich.Raoul@DESKTOP-F5E3DSV.2631.skool.net>
2018-09-20 08:22:36 +0200

    Collatz Folge

--- src/Problem.java
+++ src/Problem.java
@@ -1,14 +1,30 @@
-
 public class Problem {
 
-	public static void main(String[] args) {
-		int zahl = 9; 
-		while(zahl>1){
+	
+	public static void main(String args[]){
+	  
+    int zahl;   // Initialisierung der Variable, in der die Collatzberechnung stattfindet	
+	
+    
+    
+    
+    
+	for(int i = 5; i <=100;i++){ //Variable int = 5 wird hinzugefügt und solange hochgezählt bis i 100 erreicht
+			zahl = i;
+		  System.out.print("Seed: "+i+"| ");
+			
+			while(zahl>1){
+		  
 		if(zahl % 2 != 0){
-			zahl= zahl*3+1;
-		}
-		else{zahl=zahl/2;}
-		System.out.print(zahl+", ");
-	}}
-
+			zahl = zahl*3+1;
+			}
+		else{zahl = zahl/2;}
+		
+	   System.out.print(zahl+", ");
+	 
+	   
+	   }
+	  	System.out.println("");
+	}
+  } 
 }
