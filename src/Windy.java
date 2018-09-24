40c383f4faa2a7c0cb2844d7bf96b8fc86bd1d39
Friedrich.Raoul <Friedrich.Raoul@SK-E840F290671D.2631.skool.net>
2018-09-24 15:27:34 +0200

    Windstärke

---
+++ src/Windy.java
@@ -0,0 +1,32 @@
+import java.util.Scanner ;
+public class Windy {
+	
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hier kmh eingeben:  ");
+		int kmh = s.nextInt();
+		System.out.println("Knoten:      "+getknots(kmh));
+		System.out.println("Beaufort:    "+beaufort(kmh));
+		if(kmh<=2){
+			System.out.println("Windstärke:  Windstill");
+			
+		}
+		else{ if(kmh<100){
+			System.out.println("Windtsärke:  Normal");}}
+		
+		if(kmh>100){
+			System.out.println("Windstärke:  ORKAN");
+		}}
+		
+
+	
+	public static double getknots(double kmh){
+		return (kmh*0.54);
+	}
+	public static double beaufort(double kmh){
+		return Math.round(Math.pow((kmh/3.6)/0.836,0.6666667));
+	}
+	
+
+	}
