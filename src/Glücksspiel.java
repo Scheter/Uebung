3af6a1015189d9831761fc066953c0c2773b1cc4
Friedrich.Raoul <Friedrich.Raoul@DESKTOP-GQ1I3O9.2631.skool.net>
2018-10-18 08:13:44 +0200

    Glücksspiel Endversion

--- src/Glücksspiel.java
+++ src/Glücksspiel.java
@@ -1,34 +1,91 @@
+import java.util.Scanner;
 
 public class Glücksspiel {
-	 static boolean geschlecht = true;
-	 static int alter = 21;
-	 static String name = "Meghan";
-	 
-		public static void main(String[] args) {
-			if(alter < 18){
-				System.out.println("Hallo " + name + "!");
-				System.out.println("Du hast  " +wuerfeln());
-			}else if (!geschlecht){ 
-				System.out.println("Lieber Herr "+name +"!");
-				System.out.println("Sie haben  " +wuerfeln());
-			}else{
-				System.out.println("Liebe Frau  "+ name +"!");
-				System.out.println("Sie haben  " +wuerfeln());
-			}
+ 
+	static int alter1 = 19;
+	static int alter2 = 30;
+	
+	static Scanner s = new Scanner(System.in);
+	static boolean geschlecht1 = true;
+	static boolean geschlecht2 = false;
+	
+	static String name1 = "Susanne";
+	static String name2 = "Thomas";     //gangnamstyle whopwhop
+	static int zahlPlayer1; 
+	static int zahlPlayer2;
+	static int wuerfel = wuerfeln();
+	static int abstand1 = 0;
+	static int abstand2 = 0;
+	
+	
+	public static void main(String[] args) {
 		
-		}
-		
+		begruessung(alter1,geschlecht1,name1);
+		begruessung(alter2,geschlecht2,name2);
+		auswertung();
 			
 		
-			public static String wuerfeln(){
-				double random = Math.random();
-				if(random<= 0.42 ){
-				return"Verloren";
-				}
-					else{return"Gewonnen";
-			}
-				
+	}
+	
+	public static void begruessung(int alter, boolean geschlecht, String name){
+	
+		
+		if(!bestimmeAlter(alter)){
+			System.out.println("Hallo "+ name +"!");
+			System.out.println();
+			System.out.println("Gib deine Zahl ein:");
+			zahlPlayer1 = s.nextInt();
+			
+		}else if(!geschlecht){
+			System.out.println("Lieber Herr "+ name +"!");
+			System.out.println("Geben Sie Ihre Zahl ein:");
+			zahlPlayer1 = s.nextInt();
+			
+		}else{System.out.println("Liebe Frau "+ name +"!");
+		  System.out.println("Geben Sie Ihre Zahl ein:");
+			zahlPlayer1 = s.nextInt();}
+		
+	}
+		
+		
+	
+	
+	public static boolean bestimmeAlter(int alter){
+		if(alter < 18){
+			return false;}
+			else{return true;}
 	}
 
-}
- //*HA:  https://wiki.freitagsrunde.org/Javakurs/Übungsaufgaben/Glücksspiel 
+	
+	
+	public static int wuerfeln(){	
+		
+      double wert = Math.random();
+      
+      wert = wert*100;
+   
+      
+      return (int)wert ;}
+	
+     public static void auswertung(){
+    	 
+    	 System.out.println("Würfel: "+wuerfel );
+ 		
+ 		abstand1=wuerfel-zahlPlayer1;
+ 		abstand2=wuerfel-zahlPlayer2;
+ 		
+ 		if(abstand1<0){abstand1 = abstand1*(-1);}
+ 		if(abstand2<0){abstand2 = abstand2*(-1);}
+ 		
+ 		if(abstand1==abstand2){System.out.println("Gleichstand");}
+ 		else if(abstand1<abstand2){System.out.println("Sieg SpielerIn 1!");}
+ 		else {System.out.println("Sieg SpielerIn 2!");}
+ 		
+ 		
+     }
+    		  
+    	  
+      }
+	
+
+
